95fd539750a0f8104bc8f712dcdba28b492205cb
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-05 00:20:07 +0530

    learn new functions isalpha isdigit isalnum

--- string_functions2.py
+++ string_functions2.py
@@ -1,3 +1,25 @@
 # find
+# If value is not there the find shows "-1"
 w = "welcome"
 # print(w.find("e"))
+
+#Index
+# If value is not there then Index shows "Error" 
+# print(w.index('m'))
+
+# isalpha()
+# isalpha() check only  alphabet is there 
+print(w.isalpha())
+
+# isdigit()
+# isdigit() check only numbers are there.
+a = "32732"
+print(a.isdigit())
+
+# isalnum()
+
+b = "welcome123"
+c = "welcome"
+d = "1245"
+# Only special character is not allowed there 
+print(c.isalnum())
