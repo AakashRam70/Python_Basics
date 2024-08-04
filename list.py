7841750a118a625e62c9c16749807aa3787c93ec
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-05 00:56:58 +0530

    list

--- list.py
+++ list.py
@@ -1,11 +1,20 @@
+# List is a mutable data type it can change it values and update it
 # List is an ordered sequence of items
 # It is one of the most used datatype in Python and is very flexible.
 # List is in square brackets []
 # List can store any kinf of data like number, string, boolean and float
-# List is a mutable data type it can change it values and update it
 
-List = [
-    10, "Aakash", 10.5
-]
+# List = [
+#     10, "Aakash", 10.5
+# ]
 
-print(List, type(List))
+# print(List, type(List))
+
+# List1 = [
+#     2,3, "Hello", [ 3,4,5]
+# ]
+# print(List1[0:2])
+
+# l1 = [ 1, 2, 3, 4, 5]
+# print(l1[-1::-2]) reverse List
+# print(l1[::2])  increment list 
