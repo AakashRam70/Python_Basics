335c441db715ffb297d98b708a6389be43f50972
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-05 22:46:10 +0530

    stack_queue.js

--- list_iteration_over_2.py
+++ list_iteration_over_2.py
@@ -1,10 +1,16 @@
-l = [10, 20, 40 ,50]
-l1 = [1, 2, 3, 4, 33]
+# l = [10, 20, 40 ,50]
+# l1 = [1, 2, 3, 4, 33]
 
-t = len(l)
+# t = len(l)
 
-for a,b in zip(l,l1):
-    print(a,b)
+# for a,b in zip(l,l1):
+#     print(a,b)
 
-for h in range(t):
-    print(h)
+# for h in range(t):
+#     print(h)
+
+l =[]
+for a in range(1,4):
+    n=input("ENter the Value"+ str(a)+":-")
+    l.append(n)
+print(l)
---
+++ stack_queue_in_list.py
@@ -0,0 +1,2 @@
+# Stack is a linera data structure. 
+# Stores items in a Last-in/First-Out (LIFO) or First-In/Last-Out (FILO) manner. 
