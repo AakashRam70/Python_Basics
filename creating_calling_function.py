78d54e13a7ffb3094215b091601f03e5f5ed75ac
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-08 00:33:29 +0530

    creating_calling_function.py

---
+++ creating_calling_function.py
@@ -0,0 +1,19 @@
+
+# Creating Function 
+def showdata():
+    print("welcome to Python")
+
+# Calling Function 
+showdata()
+
+# Function Arguments 
+def sum(a,b):
+    print(a+b)
+sum(5,10)
+
+# Default Parameter Value 
+def sum(a,b=3):
+    print(a+b)
+sum(10)
+
+
