f2fb9a1d5102a22492f20adf9699199bcb5f4b04
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-08 00:15:38 +0530

    updating sets.py

--- set.py
+++ set.py
@@ -2,5 +2,10 @@
 # Every set element is unique (no duplicates) and must be immutablle (can't changed)
 # set is defined by { }
 
-set = {1, 2, 3, 2, 5}
+set = {1, 2, 3, 5, 7, 10, 4}
+print(set)
+
+# set functions
+
+set.pop()
 print(set)
