9a5d95fcf32356566556c13b318fe22525592b86
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-08 01:03:49 +0530

    math.py

--- math.py
+++ math.py
@@ -1,15 +1,20 @@
 import math
+# ceil will give forward value 
 a = 10.3
 print(math.ceil(a))
 
+# fabs will give always +ve value 
 b = -20
 print(math.fabs(b))
 
 c = 5
 print(math.factorial(c))
 
-d = 10
+# floor will give you back value 
+d = 10.9
 print(math.floor(d))
 
-e = 10
-print(math.gcd(e))
+e = [10,20,30,40]
+print(math.fsum(e))
+
+print(math.sqrt(9))
