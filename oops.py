cda5fec619ed99e4136db2a2901ce9a19303e01a
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-09 00:07:10 +0530

    oops.py Updating

--- oops.py
+++ oops.py
@@ -1,9 +1,26 @@
-class DemoClass:
- a=10
+# class DemoClass:
+#  a=10
 
- def sumvalue(self):
-    print(20+30)
+#  def sumvalue(self):
+#     print(20+30)
 
-demoobject = DemoClass()
-print(demoobject.a)
-demoobject.sumvalue();
+# demoobject = DemoClass()
+# print(demoobject.a)
+# demoobject.sumvalue();
+
+# Methods and Constructor 
+
+class DemoObject:
+    a =10
+    def showvalue(self):
+       self.c = self.a*self.a
+       print(self.c)
+
+    def showvalue1(self,a,b):
+        print(a+b)
+
+
+
+obj=DemoObject()
+obj.showvalue()
+obj.showvalue1(10,30)
