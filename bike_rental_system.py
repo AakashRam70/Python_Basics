7b289c3068705a4220a84d72a377e96ec183eb57
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-09 21:43:31 +0530

    bike_rental_system.py

--- bike_rental_system.py
+++ bike_rental_system.py
@@ -1,2 +1,34 @@
 class bikeRental:
-    
+    def __init__(self,stock):
+        self.stock=stock
+
+    def displayBike(self):
+        print("Total Bikes", self.stock)
+    def rentForBike(self,q):
+
+        if q<=0:
+            print("Invalid Quantity")
+        elif q>self.stock:
+            print("Enter the value (less than stock)")
+        else:
+            self.stock=self.stock-q
+            print("Total Prices",q*100)
+            print("Total Bikes",self.stock)
+
+while True:
+    obj = bikeRental(100)
+    uc = int((input(
+        '''
+1 Display Stocks
+2 Rent a Bike
+3 Exit
+'''
+    )))
+
+    if uc==1:
+        obj.displayBike()
+    elif uc==2:
+        n=int(input("Enter The Quentity :--"))
+        obj.rentForBike(n)
+    else:
+        break;
