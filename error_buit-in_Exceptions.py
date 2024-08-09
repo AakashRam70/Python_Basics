3af04c1aa3ab796dda6021c0d2c223ea9c11281d
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-08-09 23:59:56 +0530

    error_handling.py

---
+++ error_buit-in_Exceptions.py
@@ -0,0 +1,15 @@
+# These errors can be broadly classified into two classes: 
+
+# SyntaxError :- An error in the syntax of the code.
+# Logical Error :- An error in the logic of the code.
+
+a=10
+b=20
+if a==b:
+    print("Yes")
+else:
+    print("No")
+
+# print(a/0)
+l =[10,20,30]
+print(l[4])
